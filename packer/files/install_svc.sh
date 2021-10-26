7f13b3b180fdb8263203270dc7f12132c342ddd7
laborxcom <laborxcom@gmail.com>
2021-10-27 00:46:21 +0800

    Lab 007. create-reddit-vm.sh and files.

---
+++ packer/files/install_svc.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /home/ubuntu/reddit
+puma -d
--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -3,3 +3,5 @@ cd ~
 apt install -y git
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
+cat ../files/install_svc.sh | tee /home/ubuntu/install_svc.sh
+cat ../files/puma_svc | sudo tee /etc/systemd/system/puma.service
